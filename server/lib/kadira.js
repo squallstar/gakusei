8a70248743ceafc207810047d0d08edaaad58510
Nicholas Valbusa <squallstar@gmail.com>
2015-12-11 12:30:34 +0000

    running kadira at startup

--- server/lib/kadira.js
+++ server/lib/kadira.js
@@ -2,5 +2,7 @@ const KADIRA_APP_ID = Meteor.settings.kadiraAppId;
 const KADIRA_APP_SECRET = Meteor.settings.kadiraAppSecret;
 
 if (KADIRA_APP_ID && KADIRA_APP_SECRET) {
-  Kadira.connect(KADIRA_APP_ID, KADIRA_APP_SECRET);
+  Meteor.startup(function() {
+    Kadira.connect(KADIRA_APP_ID, KADIRA_APP_SECRET);
+  });
 }
